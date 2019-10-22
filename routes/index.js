f1e460b1da85ae21f668a3ba6058e2a5fe6586aa
ovuncalgintatlici <43188371+ovuncalgintatlici@users.noreply.github.com>
2019-10-21 23:06:03 -0400

    this is second commit

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 /*
  * GET home page.
  */
-import express = require('express');
+const express = require("express");
 const router = express.Router();
-
 router.get('/', function (req, res) {
     res.render('index', { title: 'Ovunc Algin Tatlici' });
 });
-
 router.get('/homepage', function (req, res) {
     res.render('homepage', { title: 'Home' });
 });
-
 router.get('/aboutme', function (req, res) {
     res.render('aboutme', { title: 'About Me' });
 });
-
-
 router.get('/projects', function (req, res) {
     res.render('projects', { title: 'Projects' });
 });
-
 router.get('/services', function (req, res) {
     res.render('services', { title: 'Services' });
 });
-
 router.get('/contactme', function (req, res) {
     res.render('contactme', { title: 'Contact Me' });
 });
-
 router.get('/template', function (req, res) {
     res.render('template', { title: 'Template' });
 });
-
-export default router;
+exports.default = router;
+//# sourceMappingURL=index.js.map
